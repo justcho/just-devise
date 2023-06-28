fa54ad9792234f60d24132f1aace92b7d3b21d9f
just <1620845677@qq.com>
2023-06-28 17:59:15 +0800

    文档修改

--- docs/components/switchDemo.vue
+++ docs/components/switchDemo.vue
@@ -1,5 +1,5 @@
 <template>
-  <Switch v-model:value="y" />
+  <Switch v-model:value="bool" />
 </template>
 <script lang="ts">
 import Switch from "../lib/Switch.vue";
@@ -7,8 +7,8 @@ import { ref } from "vue";
 export default {
   components: { Switch },
   setup() {
-    const y = ref(false);
-    return { y };
+    const bool = ref(false);
+    return { bool };
   },
 };
 </script>
